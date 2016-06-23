dda5403d3caa2ad8a0d26e5d418f1abbed82f5b3
ian-hall <ian-hall@users.noreply.github.com>
2016-06-23 10:40:07 -0700

    added new backgrounds and a background slideshow, changed audio controls

---
+++ scripts/audioplayer.js
@@ -0,0 +1,17 @@
+﻿var soma = document.getElementById("somafm");
+var somasrc = "http://ice1.somafm.com/missioncontrol-128-mp3";
+soma.volume = 0.45;
+function playSoma() {
+    var curSrc = soma.src;
+    if (curSrc != somasrc) {
+        soma.src = somasrc;
+        soma.load();
+        soma.play();
+    }
+};
+
+function stopSoma() {
+    soma.pause();
+    soma.src = "";
+    soma.load();
+};
---
+++ scripts/slideshow.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var body = $('body');
+    var backgrounds = ['url(../images/bg1.jpg)', 'url(../images/bg2.jpg)', 'url(../images/bg3.jpg)',
+                       'url(../images/bg4.jpg)', 'url(../images/bg5.jpg)'];
+    var wait = 10000
+    function chooseElement(list) {
+        i = Math.floor(Math.random() * list.length);
+        return list[i];
+    }
+
+    function nextBackground() {
+        body.css(
+         'background-image',
+          chooseElement(backgrounds)
+       );
+
+        setTimeout(nextBackground, wait);
+    }
+    setTimeout(nextBackground, wait);
+    body.css('background-image', chooseElement(backgrounds));
+});
