ecd432f384e6828d1f9f2cb7c37dc80afab0bf5f
ian-hall <ian-hall@users.noreply.github.com>
2016-06-24 11:04:10 -0700

    now preloading images so you don't get flickering when backgrounds change

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
-    var body = $('body');
-    var backgrounds = ['url(../RandomRares/images/bg1.jpg)', 'url(../RandomRares/images/bg2.jpg)', 'url(../RandomRares/images/bg3.jpg)',
-                       'url(../RandomRares/images/bg4.jpg)', 'url(../RandomRares/images/bg5.jpg)'];
+    var body = $('body.rarebg');
+    var backgrounds = ['http://www.imhall.com/RandomRares/images/bg1.jpg', 'http://www.imhall.com/RandomRares/images/bg2.jpg', 'http://www.imhall.com/RandomRares/images/bg3.jpg',
+                       'http://www.imhall.com/RandomRares/images/bg4.jpg', 'http://www.imhall.com/RandomRares/images/bg5.jpg'];
     var wait = 10000
     function chooseElement(list) {
         i = Math.floor(Math.random() * list.length);
@@ -9,13 +9,19 @@
     }
 
     function nextBackground() {
-        body.css(
-         'background-image',
-          chooseElement(backgrounds)
-       );
-
+        var tempImage = new Image();
+        var bgUrl = chooseElement(backgrounds);
+        tempImage.src = bgUrl;
+        tempImage.onload=function () {
+            body.css(
+             'background-image',
+              'url(' + bgUrl + ')'
+           );
+        }
         setTimeout(nextBackground, wait);
     }
+
+    var bgUrl = chooseElement(backgrounds);
     setTimeout(nextBackground, wait);
-    body.css('background-image', chooseElement(backgrounds));
+    body.css('background-image', 'url(' + bgUrl + ')');
 });
