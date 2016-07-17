365c8070c7d1287e0685f9ec7ddf6f354de3525e
ian-hall <ian-hall@users.noreply.github.com>
2016-07-17 10:58:22 -0700

    added more backgrounds

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -2,7 +2,8 @@
     var body = $('body.rarebg');
     var backgrounds = ['http://www.imhall.com/RandomRares/images/bg1.jpg', 'http://www.imhall.com/RandomRares/images/bg2.jpg', 'http://www.imhall.com/RandomRares/images/bg3.jpg',
                        'http://www.imhall.com/RandomRares/images/bg4.jpg', 'http://www.imhall.com/RandomRares/images/bg5.jpg', 'http://i.imgur.com/NN882th.jpg',
-                       'http://i.imgur.com/8xORcUe.jpg', 'http://i.imgur.com/V2nAoQH.jpg'];
+                       'http://i.imgur.com/8xORcUe.jpg', 'http://i.imgur.com/V2nAoQH.jpg', 'http://i.imgur.com/vDXFyQe.jpg', 'http://i.imgur.com/2AC9cB2.jpg',
+                       'http://i.imgur.com/2tGtK7O.jpg', 'http://i.imgur.com/5k89qEc.jpg', 'http://i.imgur.com/M5Cbs1N.jpg'];
     var wait = 10000
     function chooseElement(list) {
         i = Math.floor(Math.random() * list.length);
