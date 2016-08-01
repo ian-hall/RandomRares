10a8bf4471aeadba7d14352a0549b7f5b1deee88
ian-hall <ian-hall@users.noreply.github.com>
2016-07-31 19:59:24 -0700

    added more backgrounds. changed timing on background slideshow

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -3,8 +3,10 @@
     var backgrounds = ['http://www.imhall.com/RandomRares/images/bg1.jpg', 'http://www.imhall.com/RandomRares/images/bg2.jpg', 'http://www.imhall.com/RandomRares/images/bg3.jpg',
                        'http://www.imhall.com/RandomRares/images/bg4.jpg', 'http://www.imhall.com/RandomRares/images/bg5.jpg', 'http://i.imgur.com/NN882th.jpg',
                        'http://i.imgur.com/8xORcUe.jpg', 'http://i.imgur.com/V2nAoQH.jpg', 'http://i.imgur.com/vDXFyQe.jpg', 'http://i.imgur.com/2AC9cB2.jpg',
-                       'http://i.imgur.com/2tGtK7O.jpg', 'http://i.imgur.com/5k89qEc.jpg', 'http://i.imgur.com/M5Cbs1N.jpg'];
-    var wait = 10000
+                       'http://i.imgur.com/2tGtK7O.jpg', 'http://i.imgur.com/5k89qEc.jpg', 'http://i.imgur.com/M5Cbs1N.jpg', 'http://i.imgur.com/SrTF8PX.jpg',
+                       'http://i.imgur.com/m0nS7Kc.jpg', 'http://i.imgur.com/7vZQ75o.jpg', 'http://i.imgur.com/b58muh8.jpg', 'http://i.imgur.com/Az6EOhB.jpg',
+                       'http://i.imgur.com/nK936XZ.jpg', 'http://i.imgur.com/8qmB87x.jpg', 'http://i.imgur.com/ZskDU7m.jpg'];
+    var wait = 7500
     function chooseElement(list) {
         i = Math.floor(Math.random() * list.length);
         return list[i];
