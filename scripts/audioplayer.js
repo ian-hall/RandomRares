a3f62b670372980e24a080c89519488f79214440
ian <ian-hall@users.noreply.github.com>
2018-04-20 12:35:22 -0700

    attempting to fix audio playback on iOS devices

--- scripts/audioplayer.js
+++ scripts/audioplayer.js
@@ -1,6 +1,24 @@
 ﻿var soma = document.getElementById("somafm");
+var playbtn = document.getElementById("somaplay");
+var stopbtn = document.getElementById("somastop");
 var somasrc = "http://ice1.somafm.com/missioncontrol-128-mp3";
 soma.volume = 0.45;
+var playClick = function () {
+    var curSrc = soma.src;
+    if (curSrc != somasrc) {
+        soma.src = somasrc;
+        soma.load();
+        soma.play();
+    }
+}
+var stopClick = function () {
+    soma.pause();
+    soma.src = "";
+    soma.load();
+}
+playbtn.addEventListener("click", playClick, false);
+stopbtn.addEventListener("click", stopClick, false);
+
 function playSoma() {
     var curSrc = soma.src;
     if (curSrc != somasrc) {
